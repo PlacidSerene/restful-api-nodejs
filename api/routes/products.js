55fa9123e445d034de0dc0849b2be619b965bb4a
Daniel Nguyen <dnguyen@rlliquidators.com>
2022-12-23 10:07:31 -0800

    Final project

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,132 +1,55 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Product = require("../models/product");
+const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
+const ProductsController = require("../controllers/products");
 
-router.get("/", (req, res, next) => {
-  Product.find()
-    .select("name price _id")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        products: docs.map((doc) => {
-          return {
-            name: doc.name,
-            price: doc.price,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id,
-            },
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
 });
 
-router.post("/", (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-  });
-
-  product
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Created product successfully",
-        createdProduct: {
-          name: result.name,
-          price: result.price,
-          _id: result.id,
-          request: {
-            type: "GET",
-            url: "http://localhost:3000/products/" + result._id,
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
 });
 
-router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-    .exec()
-    .then((doc) => {
-      console.log("from db", doc);
-      if (doc) {
-        res.status(200).json({
-          product: doc,
-          request: {
-            type: "GET",
-            description: "Get all products",
-            url: "http://localhost:3000/products",
-          },
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "No valid entry found for provided ID" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.get("/", ProductsController.products_get_all);
 
-router.patch("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findByIdAndUpdate({ _id: id }, { $set: req.body })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Product Updated",
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/products/" + id,
-        },
-      });
-    });
-});
+router.post(
+  "/",
+  checkAuth,
+  upload.single("productImage"),
+  ProductsController.products_create_product
+);
 
-router.delete("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.remove({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Product deleted",
-        request: {
-          type: "POST",
-          url: "http://localhost:3000/products",
-          data: {name: 'String', price: 'Number'}
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-  res.status(200).json({
-    message: "Deleteted product!",
-  });
-});
+router.get("/:productId", ProductsController.products_get_product);
+
+router.patch(
+  "/:productId",
+  checkAuth,
+  ProductsController.products_update_product
+);
+
+router.delete(
+  "/:productId",
+  checkAuth,
+  ProductsController.products_delete_product
+);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -6,19 +6,23 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user")
 const cors = require("cors");
 
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PW}@${process.env.MONGO_ATLAS_ADDRESS}`
 );
+
 app.use(cors());
 app.use(morgan("dev"));
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
